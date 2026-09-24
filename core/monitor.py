36f3e9755d8e8b9214bbc61f077b3976f5b69625
mostafasoliman-cloud <mostafasolian2080@gmail.com>
2026-09-24 22:08:18 +0300

    Fix battery alert priority and remove unused import

--- core/monitor.py
+++ core/monitor.py
@@ -1,19 +1,16 @@
-import time
-
 from core.battery import Battery
 from core.notifier import Notifier
 
 from config import (
-    LOW_BATTERY,
     CRITICAL_BATTERY,
-    FULL_BATTERY
+    FULL_BATTERY,
+    LOW_BATTERY,
 )
 
 
 class BatteryMonitor:
 
     def __init__(self, database):
-
         self.database = database
 
         self.low_notified = False
@@ -23,7 +20,6 @@ class BatteryMonitor:
         self.previous_plugged = None
 
     def check(self):
-
         info = Battery.get_info()
 
         if info is None:
@@ -32,96 +28,57 @@ class BatteryMonitor:
         percentage = info["percent"]
         plugged = info["plugged"]
 
-        # Save data
         self.database.add_log(
             percentage,
             plugged,
             info["seconds_left"]
         )
 
-        # --------------------------------
-        # Charger connected
-        # --------------------------------
-
-        if (
-            self.previous_plugged is False
-            and plugged is True
-        ):
-
+        if self.previous_plugged is False and plugged is True:
             Notifier.send(
-                "Charger Connected 🔌",
+                "Charger Connected",
                 f"Battery: {percentage}%"
             )
 
-        # --------------------------------
-        # Charger disconnected
-        # --------------------------------
-
-        if (
-            self.previous_plugged is True
-            and plugged is False
-        ):
-
+        elif self.previous_plugged is True and plugged is False:
             Notifier.send(
                 "Charger Disconnected",
                 f"Battery: {percentage}%"
             )
 
-        # --------------------------------
-        # Low battery
-        # --------------------------------
+        # Critical battery takes priority over the low-battery alert.
+        if percentage <= CRITICAL_BATTERY and not plugged:
+            if not self.critical_notified:
+                Notifier.send(
+                    "Critical Battery",
+                    f"Battery is only {percentage}%"
+                )
+                self.critical_notified = True
 
-        if percentage <= LOW_BATTERY and not plugged:
+            self.low_notified = True
+
+        elif percentage <= LOW_BATTERY and not plugged:
+            self.critical_notified = False
 
             if not self.low_notified:
-
                 Notifier.send(
-                    "Low Battery ⚠️",
+                    "Low Battery",
                     f"Battery is {percentage}%"
                 )
-
                 self.low_notified = True
 
         else:
-
             self.low_notified = False
-
-        # --------------------------------
-        # Critical battery
-        # --------------------------------
-
-        if percentage <= CRITICAL_BATTERY and not plugged:
-
-            if not self.critical_notified:
-
-                Notifier.send(
-                    "Critical Battery 🚨",
-                    f"Battery is only {percentage}%"
-                )
-
-                self.critical_notified = True
-
-        else:
-
             self.critical_notified = False
 
-        # --------------------------------
-        # Full battery
-        # --------------------------------
-
         if percentage >= FULL_BATTERY and plugged:
-
             if not self.full_notified:
-
                 Notifier.send(
-                    "Battery Full 🔋",
+                    "Battery Full",
                     "Battery reached 100%."
                 )
-
                 self.full_notified = True
-
         else:
-
             self.full_notified = False
 
         self.previous_plugged = plugged
