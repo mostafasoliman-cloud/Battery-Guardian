fe06a74c8ba748f298ed5a6e78dcd538da2be4ab
mostafasoliman-cloud <mostafasolian2080@gmail.com>
2026-09-24 22:08:30 +0300

    Clean up application entry point

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
-from database.db import Database
 from core.monitor import BatteryMonitor
+from database.db import Database
 from gui.dashboard import Dashboard
 
 
 def main():
-
     database = Database()
-
-    monitor = BatteryMonitor(
-        database
-    )
-
-    app = Dashboard(
-        monitor
-    )
-
+    monitor = BatteryMonitor(database)
+    app = Dashboard(monitor)
     app.mainloop()
 
 
