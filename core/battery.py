ea8c69466d83a2bbea11846ef60c4a8cb4501cb6
mostafasoliman-cloud <mostafasolian2080@gmail.com>
2026-09-24 22:08:04 +0300

    Clean up core/battery.py

--- core/battery.py
+++ core/battery.py
@@ -5,7 +5,6 @@ class Battery:
 
     @staticmethod
     def get_info():
-
         battery = psutil.sensors_battery()
 
         if battery is None:
@@ -19,11 +18,10 @@ class Battery:
 
     @staticmethod
     def get_time_left(seconds):
-
-        if seconds <= 0:
+        if seconds is None or seconds <= 0:
             return "Unknown"
 
-        hours = seconds // 3600
-        minutes = (seconds % 3600) // 60
+        hours, remainder = divmod(seconds, 3600)
+        minutes = remainder // 60
 
         return f"{hours}h {minutes}m"
